dcb23658a70e37ed817289527ba0beae21effc74
Andrey Piarouski <EstSkif@gmail.com>
2022-11-05 15:02:47 +0400

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fsPromises = require('fs/promises');
+const fDirectory = path.join(__dirname, 'secret-folder');
+const readDir = async (path) => {
+  return fsPromises.readdir(path, {
+    withFileTypes: true,
+  });
+};
+const sizer = async (fPath) => {
+  return fsPromises.stat(fPath);
+};
+const isFile = (stats) => {
+  return stats.isFile();
+};
+const getName = (fPath) => {
+  return path.parse(fPath).name;
+};
+const getType = (fPath) => {
+  return path.extname(fPath).split('.')[1];
+};
+const getSize = (stats) => {
+  const bytes = stats.size;
+  const kilobytes = bytes / 1000;
+  return `${kilobytes}kb`;
+};
+const outConcat = async (fDirectory) => {
+  const files = await readDir(fDirectory);
+  for (const file of files) {
+    const fPath = path.join(fDirectory, file.name);
+    const stats = await sizer(fPath);
+    if (isFile(stats)) {
+      console.log(
+        `${getName(fPath)} - ${getType(fPath)} - ${getSize(stats)}`
+      );
+    }
+  }
+};
+
+
+outConcat(fDirectory);
